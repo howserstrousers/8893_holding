97c72d0bef851218070e33a6f051af4aaea623b2
howserstrousers <fritzcharleshowser@gmail.com>
2019-06-10 18:37:14 -0700

    images

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
-$(window).load(function() {
+$(document).ready(function () {
 
 $('.main-carousel').flickity({
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  autoPlay: 2000,
+  wrapAround:true,
+  prevNextButtons: false,
+  pageDots: false,
+  fade: true,
 });
 
 });
